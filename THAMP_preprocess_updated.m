79ba536ea4c04938b87c30336bcc6efb5ecded52
Arun Asthagiri <arun@Aruns-MacBook-Pro-3.local>
2024-10-09 10:57:30 -0400

    PLVs

--- THAMP_preprocess_updated.m
+++ THAMP_preprocess_updated.m
@@ -2,27 +2,32 @@
 % Jakob pre-Jul 2024
 % Arun Jul 2024
 %% Hyperparameters
+raw_path = "/work/mindlab/Projects/THAMP/EEG Data/raw/"; % path to raw data participant folders /THAMP/EEG_Data/Raw/
 
-participantID = "231205KKAT";
+save_path = "/work/mindlab/Projects/THAMP/EEG Data/preprocessed/new_preprocess_batch"; % path to preprocessed data participant folders /THAMP/EEG_Data/Preprocessed/
 
-
-raw_path = "<path_to_THAMP>/THAMP/EEG_Data/Raw/"; % path to raw data participant folders /THAMP/EEG_Data/Raw/
-
-save_path = "<path_to_THAMP>/THAMP/EEG_Data/Preprocessed/"; % path to preprocessed data participant folders /THAMP/EEG_Data/Preprocessed/
-
-chanlocs_filepath = "<path_to_EEGLab>/EEGlab/plugins/dipfit/standard_BEM/elec/standard_1005.elc"; %chanlocs path
+eeglab_path = '/work/mindlab/Programs/eeglab2021.1/';
+chanlocs_filepath = [eeglab_path,'plugins/dipfit4.3/standard_BEM/elec/standard_1005.elc']; %chanlocs path
 
 chans_to_load = 1:64;
 resample_rate = 500; % SHOULD THIS BE 1000Hz? Raw data seems to be at 5000Hz sampling rate
 filter_cutoffs = [0.5 60];
 rereference_chans = [10 21];
 
+addpath(eeglab_path, raw_path);
+
+%% determines subjects not yet preprocessed
+to_process= {dir(raw_path).name}; to_process = string(to_process(~contains(to_process, ".")));
+already_processed = [string({dir(save_path).name}), string({dir(fullfile(save_path, "..")).name})]; already_processed = already_processed(~contains(already_processed, "."));
+to_process = setdiff(to_process, already_processed);
+disp(to_process);
+%% Change participant ID
+participantID = "240424YDEN";
 mkdir(fullfile(save_path, participantID));
 
 % add eeglab to your matlab path :)
 
 %% 1. Load EEG file
-
 raw_data_path = fullfile(raw_path, participantID);
 raw_data_filename = strcat(participantID, '.vhdr');
 
@@ -44,7 +49,6 @@ preprocess_steps_completed = 'resampled';
 EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_steps_completed, '.set')),'filepath',char(fullfile(save_path, participantID)));
 
 %% 5. Filter (and save)
-
 EEG = pop_eegfiltnew(EEG, 'locutoff',filter_cutoffs(1),'hicutoff',filter_cutoffs(2));
 preprocess_steps_completed = 'resampled_filtered';
 EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_steps_completed, '.set')),'filepath',char(fullfile(save_path, participantID)));
@@ -64,8 +68,8 @@ pop_eegplot(EEG, 1, 1, 1); figure; topoplot([],EEG.chanlocs, 'style', 'blanEEk',
 
 
 %% 9. Manually reject bad channels (and save)
-chans_to_remove = [11 12]; % CHANGE THIS
-EEG = pop_select( EEG, 'rmchannel',chans_to_remove);
+chans_to_remove = {'AF3', 'F7'}; % CHANGE THIS
+EEG = pop_select( EEG, 'nochannel',chans_to_remove);
 
 preprocess_steps_completed = 'resampled_filtered_reref_cleaned';
 EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_steps_completed, '.set')),'filepath',char(fullfile(save_path, participantID)));
@@ -73,6 +77,8 @@ EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_st
 %% 10. Interpolate missing channels (and save)
 
 EEG = pop_interp(EEG, all_chanlocs, 'spherical');
+preprocess_steps_completed = 'resampled_filtered_reref_cleaned_interp';
+EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_steps_completed, '.set')),'filepath',char(fullfile(save_path, participantID)));
 
 %% repeat steps 8 through 10 until all bad channels are interpolated
 
@@ -80,14 +86,15 @@ EEG = pop_interp(EEG, all_chanlocs, 'spherical');
 
 EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','on');
 EEG = pop_iclabel(EEG, 'default');
-pop_viewprops(EEG, 0, 1:28); %% you may need to use the eeglab gui -- tools/Classify Components using ICLabel/Label Components
+% pop_viewprops(EEG, 0, 1:28); %% you may need to use the eeglab gui -- tools/Classify Components using ICLabel/Label Components
+%%
+classes = EEG.etc.ic_classification.ICLabel.classes;
 
-
-
+classifications = EEG.etc.ic_classification.ICLabel.classifications;
+% find components that are better explained by artifacts rather than brain data
+components_to_remove = find(any(classifications(:, ~strcmp(string(classes), 'Brain')) > classifications(:, strcmp(string(classes), 'Brain')), 2));
 %% Delete unwanted (any non-brain) components (and save)
-components_to_remove = [2 4 5]; % CHANGE THIS
 EEG = pop_subcomp( EEG, components_to_remove, 0);
 
 preprocess_steps_completed = 'resampled_filtered_reref_cleaned_interp_ica';
 EEG = pop_saveset( EEG, 'filename',char(strcat(participantID, '_', preprocess_steps_completed, '.set')),'filepath',char(fullfile(save_path, participantID)));
-
